9f66122358ca6bd832b97dc26740bd4f3536c0f6
Valdir Silva <valdir.souza_actdigital@prestador.globo>
2022-12-22 10:26:49 -0300

    feat: loading count properties by city

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -4,22 +4,20 @@ import "./featured.css";
 
 const Featured = () => {
   const { data, loading, error } = useFetch(
-    "hotels/countByCityName/?cities=london,rome"
+    "hotels/countByCityName/?cities=london,rome,berlim"
   );
-
-  console.log("Featured => ", data, loading, error);
 
   return (
     <div className="featured">
-      <div className="featuredItem">
+      {loading ? "Loading please wait..." : <><div className="featuredItem">
         <img
           src="https://cf.bstatic.com/xdata/images/city/max500/957801.webp?k=a969e39bcd40cdcc21786ba92826063e3cb09bf307bcfeac2aa392b838e9b7a5&o="
           alt=""
           className="featuredImg"
         />
         <div className="featuredTitles">
-          <h1>Dublin</h1>
-          <h2>123 properties</h2>
+          <h1>London</h1>
+          <h2>{data[0]} properties</h2>
         </div>
       </div>
 
@@ -30,8 +28,8 @@ const Featured = () => {
           className="featuredImg"
         />
         <div className="featuredTitles">
-          <h1>Reno</h1>
-          <h2>533 properties</h2>
+          <h1>Rome</h1>
+          <h2>{data[1]} properties</h2>
         </div>
       </div>
       <div className="featuredItem">
@@ -41,10 +39,10 @@ const Featured = () => {
           className="featuredImg"
         />
         <div className="featuredTitles">
-          <h1>Austin</h1>
-          <h2>532 properties</h2>
+          <h1>Berlim</h1>
+          <h2>{data[2]} properties</h2>
         </div>
-      </div>
+      </div></>}
     </div>
   );
 };
