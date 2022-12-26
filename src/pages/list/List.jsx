2d9cd6a07155469136460c06f9ca24c0ddcb1b65
Valdir Silva <valdir.souza_actdigital@prestador.globo>
2022-12-26 16:24:06 -0300

    chore: add useEffect

--- src/pages/list/List.jsx
+++ src/pages/list/List.jsx
@@ -2,7 +2,7 @@ import "./list.css";
 import Navbar from "../../components/navbar/Navbar";
 import Header from "../../components/header/Header";
 import { useLocation } from "react-router-dom";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { format } from "date-fns";
 import { DateRange } from "react-date-range";
 import SearchItem from "../../components/searchItem/SearchItem";
